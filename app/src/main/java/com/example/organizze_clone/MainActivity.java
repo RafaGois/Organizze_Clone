bc224c4dc27cbd1bbc7f3732524f96031bd98724
MendesMaquiansPCP <rafael.gois@mendesmaquinas.com.br>
2022-10-17 17:26:38 -0300

    update config slider

--- app/src/main/java/com/example/organizze_clone/MainActivity.java
+++ app/src/main/java/com/example/organizze_clone/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
