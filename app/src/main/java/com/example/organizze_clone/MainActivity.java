72160d345943ab12402b5c6e53efa5c9706be155
MendesMaquiansPCP <rafael.gois@mendesmaquinas.com.br>
2022-10-17 17:55:31 -0300

    update fragment

--- app/src/main/java/com/example/organizze_clone/MainActivity.java
+++ app/src/main/java/com/example/organizze_clone/MainActivity.java
@@ -5,14 +5,67 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.firebase.database.FirebaseDatabase;
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends IntroActivity {
 
     FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_1)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_2)
+                .build()
+        );
+
+/*
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Título 1")
+                .description("Descricao 1")
+                .image(R.drawable.um)
+                .background(android.R.color.holo_blue_bright)
+                .build());
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Título 2")
+                .description("Descricao 2")
+                .image(R.drawable.dois)
+                .background(android.R.color.holo_blue_dark)
+                .build());
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Título 3")
+                .description("Descricao 3")
+                .image(R.drawable.tres)
+                .background(android.R.color.holo_blue_light)
+                .build());
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Título 4")
+                .description("Descricao 4")
+                .image(R.drawable.quatro)
+                .background(android.R.color.holo_orange_light)
+                .build());
+
+ */
+
     }
 }
