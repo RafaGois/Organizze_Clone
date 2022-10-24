142a6e75b717ee1d6a2f5ce9600ddebaa2166aca
MendesMaquiansPCP <rafael.gois@mendesmaquinas.com.br>
2022-10-24 07:45:18 -0300

    update nova versão android e classes bo base64

--- app/src/main/java/com/example/organizze_clone/helper/Base64Custom.java
+++ app/src/main/java/com/example/organizze_clone/helper/Base64Custom.java
@@ -1,15 +1,15 @@
 package com.example.organizze_clone.helper;
 
-import java.util.Base64;
+import android.util.Base64;
 
 public class Base64Custom {
 
     public static String codificarBase64 (String texto) {
-
+        return Base64.encodeToString(texto.getBytes(),Base64.DEFAULT).replaceAll("(\\n|\\r)","");
     }
 
     public static String decodificarBase64 (String texto) {
-
+        return new String(Base64.decode(texto, Base64.DEFAULT));
     }
 
 }
